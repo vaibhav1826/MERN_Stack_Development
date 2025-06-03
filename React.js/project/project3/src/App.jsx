bd0b5d442a23e78209b8274bd2bcfa57106707d2
Vaibhav bhatt <148764896+vaibhav1826@users.noreply.github.com>
2025-06-03 13:52:44 +0530

    project3

--- React.js/project/project3/src/App.jsx
+++ React.js/project/project3/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
-      
+    <Header />
+    <Home />
+    <Footer />
     </>
   )
 }
--- React.js/project/project3/src/main.jsx
+++ React.js/project/project3/src/main.jsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About.jsx'
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children:[{
+      path:'',
+      element: <Home />
+    },{
+      path:"about",
+      element: <About />
+    }]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
